f21e9439c486bbf314fb34e3908c261f746a925d
Arttu Henriksson <ad1820@student.jamk.fi>
2024-11-12 17:36:20 +0200

    added RepoCard component

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,9 +1,13 @@
 // Projects.tsx
 import React from 'react';
-//import RepoCard from '../components/projects/RepoCard';
+import RepoCard from '../components/projects/RepoCard';
 
 const Projects: React.FC = () => {
-  return <></>;
+  return (
+    <>
+      <RepoCard />
+    </>
+  );
 };
 
 export default Projects;
