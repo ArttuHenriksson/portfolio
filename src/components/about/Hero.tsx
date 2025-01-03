a5f51593e02f7def803b561c476c60148546fd76
Arttu Henriksson <ad1820@student.jamk.fi>
2025-01-03 18:54:42 +0200

    removed the lorem

--- src/components/about/Hero.tsx
+++ src/components/about/Hero.tsx
@@ -4,12 +4,7 @@ function Hero() {
       <div className="flex flex-col items-center text-white">
         <div className="flex flex-col md:flex-row items-center mt-12 max-w-4xl w-full">
           <div className="flex items-center justify-center text-center">
-            <p className="text-lg  p-4 rounded-lg text-white">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
-              tempora quo quidem praesentium nihil, tempore numquam eligendi sed
-              molestias saepe, cumque inventore. Possimus iure eius, nam maxime
-              temporibus expedita quo.
-            </p>
+            <p className="text-lg  p-4 rounded-lg text-white"></p>
           </div>
         </div>
       </div>
