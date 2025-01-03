b16f6fbe7ed8f587931861dcf99ce5546bb6c6dd
Arttu Henriksson <ad1820@student.jamk.fi>
2025-01-03 18:56:58 +0200

    added more natural tone greetings.

--- src/utils/greetings.ts
+++ src/utils/greetings.ts
@@ -1,34 +1,32 @@
 export function getGoodMorningGreeting() {
   const greetings = [
-    'Good morning, you glorious morning person!',
-    'Rise and shine, you radiant morning star!',
-    'Top of the morning to you, sunshine!',
-    'Wishing you a fantastic start to the day, you morning legend!',
-    'Good morning, you unstoppable dawn dynamo!',
+    'Good morning!',
+    'Rise and shine!',
+    'Top of the morning to you!',
+    'Wishing you a great start to the day!',
+    'Good morning, have a wonderful day ahead!',
   ];
   return greetings[Math.floor(Math.random() * greetings.length)];
 }
 
 export function getGoodAfternoonGreeting() {
   const greetings = [
-    'Afternoon, you magnificent creature!',
-    'Good day, you afternoon champion!',
-    'Salutations, you midday marvel!',
-    'Hello, you afternoon wonder!',
-    'Hope your afternoon is as amazing as you are!',
-    'Good afternoon, you blazing afternoon beacon!',
+    'Good afternoon!',
+    'Hope you’re having a great afternoon!',
+    'Hello, good afternoon!',
+    'Wishing you a pleasant afternoon!',
+    'Good afternoon, enjoy the rest of your day!',
   ];
   return greetings[Math.floor(Math.random() * greetings.length)];
 }
 
 export function getGoodEveningGreeting() {
   const greetings = [
-    'Good evening, you dazzling nighthawk!',
-    "Well, if it isn't my favorite evening enthusiast.",
-    'Evening, you glorious twilight titan!',
-    'Greetings, you magnificent moonlit marvel!',
-    'Good evening, you spectacular starlit soul!',
-    'Nightfall greetings, you fabulous evening explorer!',
+    'Good evening!',
+    'Hope you had a great day!',
+    'Evening, how was your day?',
+    'Good evening, relax and unwind!',
+    'Good evening, enjoy your night!',
   ];
   return greetings[Math.floor(Math.random() * greetings.length)];
 }
