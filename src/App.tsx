0cc2302081a5b912df20b3ee0bb2c43bce7ffdfd
Arttu Henriksson <ad1820@student.jamk.fi>
2025-02-11 14:14:36 +0200

    removed the snow-overlay

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,12 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
-import { SnowOverlay } from 'react-snow-overlay';
+
 
 const App: React.FC = () => {
   return (
     <div className="flex flex-col min-h-screen bg-zinc-900 relative">
-      <SnowOverlay />
+ 
       <Header />
       <div className="flex-1 relative">
         <main className="flex-1 px-4 mt-16 mb-16">
